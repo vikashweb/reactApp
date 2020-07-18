efc80c5532a2a5e0b0125c6f0e7d2b040b84f5c8
vikash rawat <vickeyrawat111@gmail.com>
2020-07-19 05:19:47 +1400

    Updates

---
+++ precache-manifest.4f150ef44b8e99d6676704815210b6c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a028842a370fdb0042cfd0a16e9413",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "99d06562d2466c914dd2",
+    "url": "/reactApp/static/js/2.66e4e005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.66e4e005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34b9260d2551338559d",
+    "url": "/reactApp/static/js/main.42bd0486.chunk.js"
+  },
+  {
+    "revision": "2ccf203fb42d4f55ad7a",
+    "url": "/reactApp/static/js/runtime-main.359228ba.js"
+  }
+]);
